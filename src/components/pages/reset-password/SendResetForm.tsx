d34aa64f9267e543aaf47842f207131446755028
snordale <snordale@gmail.com>
2022-12-12 00:48:05 -0500

    rm graphql call

--- src/components/pages/reset-password/SendResetForm.tsx
+++ src/components/pages/reset-password/SendResetForm.tsx
@@ -1,30 +1,29 @@
 import * as yup from "yup";
 
-import { Stack, TextField } from "@mui/material";
 import { Form, Formik } from "formik";
+import { Stack, TextField } from "@mui/material";
 
 import { LoadingButton } from "@mui/lab";
 import React from "react";
 import toast from "react-hot-toast";
-import { useSendPasswordResetMutation } from "../../../generated/graphql";
 
 const validationSchema = yup.object({
 	email: yup.string().min(6, "Minimum 6 characters length").required(),
 });
 
 export const SendResetForm: React.FC = () => {
-	const [, sendPasswordReset] = useSendPasswordResetMutation();
+	//const [, sendPasswordReset] = useSendPasswordResetMutation();
 	return (
 		<Formik
 			initialValues={{ email: "" }}
 			validationSchema={validationSchema}
 			onSubmit={async (data, { setErrors }) => {
-				const res = await sendPasswordReset({ ...data });
-				if (!res.data?.sendPasswordReset) {
-					toast.error("Something went wrong.");
-				} else {
-					toast.success("Email sent.");
-				}
+				//const res = await sendPasswordReset({ ...data });
+				//if (!res.data?.sendPasswordReset) {
+				//	toast.error("Something went wrong.");
+				//} else {
+				//	toast.success("Email sent.");
+				//}
 				// is sent --- TODO validate email on backend
 			}}
 		>
